9c58b9b8b476d1286a6aa524974f91ef4b619d5f
beverlyjaimes <beverly.jaimespuente@gmail.com>
2019-12-13 12:19:25 -0600

    finished exercise syntax, types, variables

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,31 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+    double myFavoriteNumber = 3.14;
+        System.out.println("myFavoriteNumber = " + myFavoriteNumber);
+    String myString = "I love Marissa";
+        System.out.println("myString = " + myString);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+        int y = 5;
+        System.out.println(++y);
+        System.out.println(y);
+
+        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//
+//        int three = (int) "three";
+
+        int x = 4 + 5;
+        System.out.println("x = " + x);
+        int b = 5;
+
+
+
     }
+    
 }
