b47c26ef5c5a46b57abf085dd15400ebbfd3067b
beverlyjaimes <beverly.jaimespuente@gmail.com>
2020-01-10 16:13:19 -0600

    exercise two

--- src/codingbat_exercises.java
+++ src/codingbat_exercises.java
@@ -1,6 +1,7 @@
 public class codingbat_exercises {
     public static void main(String[] args) {
         System.out.println(sleepIn(false, false));
+        System.out.println(diff21(5));
     }
 
 
@@ -20,5 +21,21 @@ public class codingbat_exercises {
             return true;
         }
         return false;
+    }
+
+//
+//    Given an int n, return the absolute difference between n and 21, except return double the absolute difference if n is over 21.
+//
+//
+//    diff21(19) → 2
+//    diff21(10) → 11
+//    diff21(21) → 0
+
+    public static int diff21(int n) {
+        if (n <= 21) {
+            return 21 - n;
+        } else {
+            return (n - 21) * 2;
+        }
     }
 }
