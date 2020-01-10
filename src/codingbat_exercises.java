1d84a29586428bf6d722743905a7d4c2cf71a0aa
beverlyjaimes <beverly.jaimespuente@gmail.com>
2020-01-10 16:06:15 -0600

    correction to exercise 1 tested in main

--- src/codingbat_exercises.java
+++ src/codingbat_exercises.java
@@ -1,16 +1,20 @@
 public class codingbat_exercises {
-}
+    public static void main(String[] args) {
+        System.out.println(sleepIn(false, false));
+    }
+
+
 
 
 //    The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation. We sleep in if it is not a weekday or we're on vacation. Return true if we sleep in.
 //
-//
-//        sleepIn(false, false) → true
-//        sleepIn(true, false) → false
-//        sleepIn(false, true) → true
+////        sleepIn(false, false) → true
+////        sleepIn(true, false) → false
+////        sleepIn(false, true) → true
+
 //
 
-    public boolean sleepIn(boolean weekday, boolean vacation) {
+    public static boolean sleepIn(boolean weekday, boolean vacation) {
 
         if(!weekday || vacation){
             return true;
