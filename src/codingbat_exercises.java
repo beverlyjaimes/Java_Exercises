47326e602fa1d20355150c45637585363117aada
beverlyjaimes <beverly.jaimespuente@gmail.com>
2020-01-10 16:27:01 -0600

    exercise three

--- src/codingbat_exercises.java
+++ src/codingbat_exercises.java
@@ -2,6 +2,7 @@ public class codingbat_exercises {
     public static void main(String[] args) {
         System.out.println(sleepIn(false, false));
         System.out.println(diff21(5));
+        System.out.println(nearHundred(60));
     }
 
 
@@ -37,5 +38,18 @@ public class codingbat_exercises {
         } else {
             return (n - 21) * 2;
         }
+    }
+
+
+
+//    Given an int n, return true if it is within 10 of 100 or 200. Note: Math.abs(num) computes the absolute value of a number.
+//
+//
+//        nearHundred(93) → true
+//        nearHundred(90) → true
+//        nearHundred(89) → false
+
+    public static boolean nearHundred(int n) {
+        return ((Math.abs(100 - n) <= 10) || (Math.abs(200 - n) <= 10));
     }
 }
