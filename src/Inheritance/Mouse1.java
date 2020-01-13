adec7461309768cec013ca8366512e31a5c33943
beverlyjaimes <beverly.jaimespuente@gmail.com>
2020-01-12 21:06:05 -0600

    generic mouse created

--- src/Inheritance/Mouse1.java
+++ src/Inheritance/Mouse1.java
@@ -4,13 +4,6 @@ public class Mouse1 {
 
     String texture = "Matte";
 
-    public static void leftClick() {
-        System.out.println("Click");
-
-    }
-    public static void rightClick() {
-        System.out.println("Click");
-    }
 
     public static void setColor(String color){
         System.out.println(color);
--- src/Inheritance/Mouse2.java
+++ src/Inheritance/Mouse2.java
@@ -1,13 +1,7 @@
 package Inheritance;
 
 public class Mouse2 {
-    public static void leftClick() {
-        System.out.println("Click");
 
-    }
-    public static void rightClick() {
-        System.out.println("Click");
-    }
 
     public static void connect() {
         System.out.println("Connected!");
