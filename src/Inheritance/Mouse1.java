55a543323398bf07f52dbae69e7fa88102d68577
beverlyjaimes <beverly.jaimespuente@gmail.com>
2020-01-12 21:01:50 -0600

    mouse1

--- src/Inheritance/Mouse1.java
+++ src/Inheritance/Mouse1.java
@@ -1,4 +1,18 @@
 package Inheritance;
 
 public class Mouse1 {
+
+    String texture = "Matte";
+
+    public static void leftClick() {
+        System.out.println("Click");
+
+    }
+    public static void rightClick() {
+        System.out.println("Click");
+    }
+
+    public static void setColor(String color){
+        System.out.println(color);
+    }
 }
