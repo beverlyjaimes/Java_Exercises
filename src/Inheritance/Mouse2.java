dd8288f0b3a4b2cb95510357b6ea975e38971634
beverlyjaimes <beverly.jaimespuente@gmail.com>
2020-01-12 21:03:00 -0600

    mouse2

--- src/Inheritance/Mouse2.java
+++ src/Inheritance/Mouse2.java
@@ -1,4 +1,15 @@
 package Inheritance;
 
 public class Mouse2 {
+    public static void leftClick() {
+        System.out.println("Click");
+
+    }
+    public static void rightClick() {
+        System.out.println("Click");
+    }
+
+    public static void connect() {
+        System.out.println("Connected!");
+    }
 }
