cb03ad866b9dd1c704e050f9535d67b2d23b8d3b
beverlyjaimes <beverly.jaimespuente@gmail.com>
2020-01-12 21:12:31 -0600

    testing

--- src/Inheritance/Main.java
+++ src/Inheritance/Main.java
@@ -2,6 +2,15 @@ package Inheritance;
 
 public class Main {
     public static void main(String[] args) {
+        Mouse1 m1 = new Mouse1();
+        Mouse2 m2 = new Mouse2();
+
+        m1.leftClick();
+        m1.rightClick();
+        m1.scrolDown();
+        m1.scrollUp();
+
+        m2.connect();
 
     }
 }
--- src/Inheritance/Mouse1.java
+++ src/Inheritance/Mouse1.java
@@ -4,7 +4,6 @@ public class Mouse1 extends Mouse {
 
     String texture = "Matte";
 
-
     public static void setColor(String color){
         System.out.println(color);
     }
