5e16bfc78ce76016131d36fba4aa4c64e72154e4
beverlyjaimes <beverly.jaimespuente@gmail.com>
2020-01-12 21:40:40 -0600

    main contains name

--- src/Encapsulation/Main.java
+++ src/Encapsulation/Main.java
@@ -3,5 +3,11 @@ package Encapsulation;
 public class Main {
     public static void main(String[] args) {
 
+        Student s = new Student();
+        s.setName("Joe");
+        s.setAge(5);
+
+        System.out.println("s.name = " + s.name);
+        System.out.println("s.age = " + s.age);
     }
 }
--- src/Encapsulation/Student.java
+++ src/Encapsulation/Student.java
@@ -1,4 +1,17 @@
 package Encapsulation;
 
 public class Student {
+
+    String name;
+    int age;
+
+//    no static, static means you dont have to make an object to create a method
+
+    public void setName(String newName) {
+        name = newName;
+    }
+
+
 }
+
+
